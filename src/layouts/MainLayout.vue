ef4d8de815e4a209ac58d39c70c3f2be8057ada7
Kirill B <wannnwdooo@gmail.com>
2023-06-02 08:35:35 +0300

    first commit2

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,12 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" class="layout">
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+<style lang="scss" scoped>
+.layout {
+  height: 100%;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,62 +1,44 @@
-import { store } from 'quasar/wrappers'
-import { InjectionKey } from 'vue'
-import { Router } from 'vue-router'
+import { store } from 'quasar/wrappers';
+import { InjectionKey } from 'vue';
+import { Router } from 'vue-router';
 import {
   createStore,
   Store as VuexStore,
   useStore as vuexUseStore,
-} from 'vuex'
-
-// import example from './module-example'
-// import { ExampleStateInterface } from './module-example/state';
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
+} from 'vuex';
+import { models, ModelsStateInterface } from 'src/store/modules';
 
 export interface StateInterface {
-  // Define your own store structure, using submodules if needed
-  // example: ExampleStateInterface;
-  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  example: unknown
+  models: ModelsStateInterface;
 }
 
-// provide typings for `this.$store`
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
-    $store: VuexStore<StateInterface>
+    $store: VuexStore<StateInterface>;
   }
 }
 
-// provide typings for `useStore` helper
-export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol('vuex-key')
+export const storeKey: InjectionKey<VuexStore<StateInterface>> =
+  Symbol('vuex-key');
 
-// Provide typings for `this.$router` inside Vuex stores
- declare module 'vuex' {
-   export interface Store<S> {
-     readonly $router: Router;
-   }
- }
+declare module 'vuex' {
+  export interface Store<S> {
+    readonly $router: Router;
+  }
+}
 
-export default store(function (/* { ssrContext } */) {
+export default store(function () {
   const Store = createStore<StateInterface>({
     modules: {
-      // example
+      models,
     },
 
-    // enable strict mode (adds overhead!)
-    // for dev mode and --debug builds only
-    strict: !!process.env.DEBUGGING
-  })
+    strict: !!process.env.DEBUGGING,
+  });
 
   return Store;
-})
+});
 
 export function useStore() {
-  return vuexUseStore(storeKey)
+  return vuexUseStore(storeKey);
 }
